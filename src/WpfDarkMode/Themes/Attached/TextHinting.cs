69a49a2689a3d84fccf82c366a4761fc02e0b0c0
Pao <9247381+PolkageistHS@users.noreply.github.com>
2025-09-10 21:00:16 -0700

    added actual dark theme

---
+++ src/WpfDarkMode/Themes/Attached/TextHinting.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WpfDarkMode.Themes.Attached;
+
+/// <summary>
+/// An attached property class for showing a hint when a text box is empty. Set the text box's Tag property to the text
+/// </summary>
+public static class TextHinting
+{
+    public static readonly DependencyProperty ShowWhenFocusedProperty =
+        DependencyProperty.RegisterAttached("ShowWhenFocused", typeof(bool), typeof(TextHinting), new FrameworkPropertyMetadata(false));
+
+    public static void SetShowWhenFocused(Control control, bool value)
+    {
+        if (control is not (TextBoxBase or PasswordBox))
+            throw new ArgumentException("Control was not a textbox", nameof(control));
+        control.SetValue(ShowWhenFocusedProperty, value);
+    }
+
+    public static bool GetShowWhenFocused(Control control)
+    {
+        if (control is not (TextBoxBase or PasswordBox))
+            throw new ArgumentException("Control was not a textbox", nameof(control));
+        return (bool)control.GetValue(ShowWhenFocusedProperty);
+    }
+}
---
+++ src/WpfDarkMode/Themes/Controls.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace WpfDarkMode.Themes;
+
+public partial class Controls
+{
+    private void CloseWindow_Event(object sender, RoutedEventArgs e)
+    {
+        CloseWind(Window.GetWindow((FrameworkElement)e.Source));
+    }
+
+    private void AutoMinimize_Event(object sender, RoutedEventArgs e)
+    {
+        MaximizeRestore(Window.GetWindow((FrameworkElement)e.Source));
+    }
+
+    private void Minimize_Event(object sender, RoutedEventArgs e)
+    {
+        MinimizeWind(Window.GetWindow((FrameworkElement)e.Source));
+    }
+
+    private static void CloseWind(Window? window)
+    {
+        window?.Close();
+    }
+
+    private static void MaximizeRestore(Window? window)
+    {
+        if (window == null)
+            return;
+        window.WindowState = window.WindowState switch
+        {
+            WindowState.Normal => WindowState.Maximized,
+            WindowState.Minimized or WindowState.Maximized => WindowState.Normal,
+            _ => window.WindowState
+        };
+    }
+
+    private static void MinimizeWind(Window? window)
+    {
+        if (window != null)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
